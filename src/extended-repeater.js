fd9efe3c7fd277fd1f55ec0868609a140cbde17f
YuliAdam <ylia_adamovich@mail.ru>
2025-01-01 12:53:10 +0100

    feat: add task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  if(!options.hasOwnProperty("addition")){
+    options.addition = '';
+  }
+  if(!options.hasOwnProperty( "additionSeparator")){
+    options.additionSeparator = '|';
+  }
+  if(!options.hasOwnProperty( "separator")){
+    options.separator = '+';
+  }
+  if(!options.hasOwnProperty( "repeatTimes")){
+    options.repeatTimes = 1;
+  }
+  if(!options.hasOwnProperty( "additionRepeatTimes")){
+    options.additionRepeatTimes = 1;
+  }
+
+  let partAddition = (''+ options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+  if(options.additionRepeatTimes>0){
+    partAddition = partAddition.substring(0,partAddition.length - options.additionSeparator.length);
+  }
+  let resultSt = ('' + str + partAddition +options.separator).repeat(options.repeatTimes);
+  if(options.repeatTimes>0){ 
+    resultSt = resultSt.substring(0,resultSt.length-options.separator.length); 
+  }
+  return resultSt;
 }
 
 module.exports = {
